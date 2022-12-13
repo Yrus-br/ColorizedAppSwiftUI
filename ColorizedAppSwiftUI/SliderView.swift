a6d4ab7f403cb8974b3e8ffe226104e5eee73e2a
Yurii <yourussianbrother@gmail.com>
2022-12-14 01:57:46 +0300

    added button to confirm value and alert for wrong format

--- ColorizedAppSwiftUI/SliderView.swift
+++ ColorizedAppSwiftUI/SliderView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 struct SliderView: View {
     
     @Binding var colorValue: Double
+    @State private var alert = false
     let sliderSelfColor: Color
     
     var body: some View {
@@ -19,9 +20,35 @@ struct SliderView: View {
             Slider(value: $colorValue, in: 0...255, step: 1)
                 .accentColor(sliderSelfColor)
             
-            TextField("0", value: $colorValue, format: .number)
+            HStack {
+                TextField(
+                    "0",
+                    value: $colorValue,
+                    format: .number
+                )
                 .textFieldStyle(.roundedBorder)
                 .frame(width: 50)
+                .keyboardType(.numberPad)
+                
+                Button("Done", action: checkValue)
+                    .alert("Wrong Format", isPresented: $alert, actions: {}) {
+                        Text("Please try again")
+                    }
+            }
+            
         }.padding()
+    }
+    
+    private func checkValue() {
+        
+        if colorValue > 255 {
+            colorValue = 255
+            alert.toggle()
+        }
+        
+        if colorValue < 0 {
+            colorValue = 0
+            alert.toggle()
+        }
     }
 }
